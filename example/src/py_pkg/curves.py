1426626fced8cc7cdad024aaef53ea93575331de
Duy Nguyen <duy.nguyen.public@gmail.com>
2019-04-26 10:54:19 +0300

    Update curves.py

--- example/src/py_pkg/curves.py
+++ example/src/py_pkg/curves.py
@@ -3,7 +3,7 @@ py_pkg.curves.py
 ~~~~~~~~~~~~~~~~
 
 This exaple module contains classes that model economic supply and
-demand curves and compute values such as equilibrium prices. 
+demand curves and compute values such as equilibrium prices.
 
 It also serves as a demonstration for using type annotations and
 abstract base classes in developing libraries intended for use in other
